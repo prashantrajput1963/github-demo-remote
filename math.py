a5895df33b2595ecbb92d34d58fbd68072311a35
prashantrajput1963 <98019710+prashantrajput1963@users.noreply.github.com>
2022-01-19 15:56:57 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,19 @@
+# add implementation
+def add(x,y):
+    return x+y
+     # subtract implementation
+def subtract(x,y):
+     x-y
+    # multiply implementation
+def multiply(x,y):
+    x*y
+    # divide implementation
+def divide(x,y):
+    if y==0;
+    return divide_by_0_error
+    else
+    return x/y
+
+#sqaure implementation
+    def square(x):
+        return x*x
