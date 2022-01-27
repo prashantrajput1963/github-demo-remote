42a1f83229db06de7f460fc68228b7425529153c
prashantrajput1963 <98019710+prashantrajput1963@users.noreply.github.com>
2022-01-27 12:03:57 +0530

    Add files via upload

--- math.py
+++ math.py
@@ -1,19 +1,9 @@
-# add implementation
-def add(x,y):
-    return x+y # addition
-     # subtract implementation
-def subtract(x,y):
-     x-y           # addition
-    # multiply implementation
-def multiply(x,y):
-    x*y           # addition
-    # divide implementation
-def divide(x,y):
-    if y==0;
-    return divide_by_0_error
-    else
-    return x/y      # addition
 
-#sqaure implementation
-    def square(x):
-        return x*x
+def add(x,y):
+     x+y
+def subtract(x,y):
+     x-y
+def multiply(x,y):
+    x*y
+def divide(x,y):
+   x/y
